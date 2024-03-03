a7368ce9e4e32fffc2dbcadb520e687fc5a03f30
danielmarial <d.kudum@alustudent.com>
2024-03-03 19:11:11 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,33 @@
-#!/usr/bin/python3
+ubreddit):
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    from requests import get
 
-""" place holder """
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
 
+    headers = {'user-agent': 'my-app/0.0.1'}
 
-def top_ten(subreddit):
-    import requests
-    link = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    result = requests.get(link, headers={"User-Agent": "Mozilla/5.0"})
-    result = result.json()
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        print(None)
+        return None
+
     try:
-        result = result["data"]["children"][:10]
-        for x in result:
-            print("{}".format(x["data"]["title"]))
-    except Exception as error:
-        print("None")
+        js = r.json()
+
+    except ValueError:
+        print(None)
+        return None
+
+    try:
+
+        data = js.get("data")
+        children = data.get("children")
+        for child in children[:10]:
+            post = child.get("data")
+            print(post.get("title"))
+
+    except:
+        print(None)
